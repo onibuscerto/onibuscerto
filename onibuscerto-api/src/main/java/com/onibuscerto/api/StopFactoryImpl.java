0b0bcf8c6e51ed0337830190adb1fee5ecf26a7a
Luiz Ribeiro <luizribeiro@gmail.com>
2011-07-21 14:23:31 -0300

    Criados métodos getAllStops e getStopById no StopFactory

--- onibuscerto-api/src/main/java/com/onibuscerto/api/StopFactoryImpl.java
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/StopFactoryImpl.java
@@ -2,11 +2,16 @@ package com.onibuscerto.api;
 
 import com.onibuscerto.api.factories.StopFactory;
 import com.onibuscerto.api.entities.Stop;
+import java.util.Collection;
+import java.util.LinkedList;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ReturnableEvaluator;
+import org.neo4j.graphdb.StopEvaluator;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.Traverser;
 
 class StopFactoryImpl implements StopFactory {
 
@@ -39,5 +44,30 @@ class StopFactoryImpl implements StopFactory {
         } finally {
             tx.finish();
         }
+    }
+
+    private Collection<Node> getAllStopNodes() {
+        Traverser traverser = stopFactoryNode.traverse(
+                Traverser.Order.BREADTH_FIRST, StopEvaluator.DEPTH_ONE,
+                ReturnableEvaluator.ALL_BUT_START_NODE,
+                Relationships.STOP, Direction.OUTGOING);
+        return traverser.getAllNodes();
+    }
+
+    @Override
+    public Collection<Stop> getAllStops() {
+        Collection<Node> allStopNodes = getAllStopNodes();
+        Collection<Stop> allStops = new LinkedList<Stop>();
+
+        for (Node node : allStopNodes) {
+            allStops.add(new StopImpl(node));
+        }
+
+        return allStops;
+    }
+
+    @Override
+    public Stop getStopById(String id) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
--- onibuscerto-api/src/main/java/com/onibuscerto/api/factories/StopFactory.java
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/factories/StopFactory.java
@@ -1,8 +1,13 @@
 package com.onibuscerto.api.factories;
 
 import com.onibuscerto.api.entities.Stop;
+import java.util.Collection;
 
 public interface StopFactory {
 
     public Stop createStop();
+
+    public Collection<Stop> getAllStops();
+
+    public Stop getStopById(String id);
 }
