c81ada9a37eb23574b05764bc0a262df89b9e8fc
Lucas Campiolo Paiva <lucascpaiva@gmail.com>
2011-07-22 15:19:04 -0300

    adicionei um getAllTrips

--- onibuscerto-api/src/main/java/com/onibuscerto/api/factories/TripFactory.java
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/factories/TripFactory.java
@@ -1,10 +1,13 @@
 package com.onibuscerto.api.factories;
 
 import com.onibuscerto.api.entities.Trip;
+import java.util.Collection;
 
 public interface TripFactory {
 
     public Trip createTrip(String id);
+
+    public Collection<Trip> getAllTrips();
 
     public Trip getTripById(String id);
 }
