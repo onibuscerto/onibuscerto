b24806654a3f71e2179d0afc785051abdb70b0b9
Lucas Campiolo Paiva <lucascpaiva@gmail.com>
2011-07-22 15:44:39 -0300

    adicionei método que liga os StopTimes de cada Trip

--- onibuscerto-importer/src/main/java/com/onibuscerto/importer/ImporterMain.java
+++ onibuscerto-importer/src/main/java/com/onibuscerto/importer/ImporterMain.java
@@ -1,6 +1,12 @@
 package com.onibuscerto.importer;
 
 import com.onibuscerto.api.DatabaseController;
+import com.onibuscerto.api.entities.StopTime;
+import com.onibuscerto.api.entities.Trip;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 public class ImporterMain {
 
@@ -16,5 +22,29 @@ public class ImporterMain {
 
     public static void main(String args[]) {
         new ImporterMain();
+    }
+
+    private void linkStopTimes() {
+        Collection<Trip> allTrips = databaseController.getTripFactory().getAllTrips();
+        LinkedList<StopTime> stopTimes;
+
+        for (Trip trip : allTrips) {
+            stopTimes = new LinkedList<StopTime>(trip.getStopTimes());
+            Collections.sort(stopTimes, new Comparator() {
+
+                @Override
+                public int compare(Object o1, Object o2) {
+                    StopTime st1 = (StopTime) o1;
+                    StopTime st2 = (StopTime) o2;
+                    return ((Integer) st1.getSequence()).compareTo(st2.getSequence());
+                }
+            });
+            while (stopTimes.isEmpty() == false) {
+                StopTime stopTime = stopTimes.pop();
+                if (stopTimes.isEmpty() == false) {
+                    stopTime.setNext(stopTimes.peekFirst());
+                }
+            }
+        }
     }
 }
