d0793af7b3824414f93471886312b518b5526864
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-04 16:07:17 -0300

    Criada interface Location

---
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/entities/Location.java
@@ -0,0 +1,12 @@
+package com.onibuscerto.api.entities;
+
+public interface Location {
+
+    public double getLatitude();
+
+    public void setLatitude(double latitude);
+
+    public double getLongitude();
+
+    public void setLongitude(double longitude);
+}
