06474867e0bc8a6037a3cd1c25f3bf28bd626035
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-03 22:44:49 -0300

    Implementado rascunho da função getShortestPath do DatabaseController

--- onibuscerto-api/src/main/java/com/onibuscerto/api/DatabaseController.java
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/DatabaseController.java
@@ -1,10 +1,17 @@
 package com.onibuscerto.api;
 
+import com.onibuscerto.api.entities.Connection;
+import com.onibuscerto.api.entities.Stop;
+import com.onibuscerto.api.entities.TransportConnection;
 import com.onibuscerto.api.factories.ConnectionFactory;
 import com.onibuscerto.api.factories.RouteFactory;
 import com.onibuscerto.api.factories.StopFactory;
 import com.onibuscerto.api.factories.StopTimeFactory;
 import com.onibuscerto.api.factories.TripFactory;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
@@ -67,6 +74,61 @@ public final class DatabaseController {
         currentTransaction.finish();
 
         currentTransaction = null;
+    }
+
+    public Collection<Stop> getShortestPath(Stop source, Stop target, int time) {
+        HashMap<Stop, Integer> d = new HashMap<Stop, Integer>();
+        HashMap<Stop, Stop> p = new HashMap<Stop, Stop>();
+        HashSet<Stop> in = new HashSet<Stop>();
+        Collection<Stop> path = new LinkedList<Stop>();
+        Collection<Stop> allStops = getStopFactory().getAllStops();
+
+        for (Stop stop : allStops) {
+            d.put(stop, 0x3f3f3f3f);
+        }
+        d.put(source, time);
+
+        while (!in.contains(target)) {
+            Stop stop = null;
+            int best = 0x3f3f3f3f;
+            for (Stop s : allStops) {
+                if (!in.contains(s) && best > d.get(s)) {
+                    best = d.get(stop = s);
+                }
+            }
+
+            if (stop == null) {
+                return null;
+            }
+
+            in.add(stop);
+            time = d.get(stop);
+
+            for (Connection c : stop.getOutgoingConnections()) {
+                int waitingTime = 0;
+
+                if (c instanceof TransportConnection) {
+                    waitingTime = ((TransportConnection) c).getDepartureTime() - time;
+                    if (waitingTime <= 0) {
+                        continue;
+                    }
+                }
+
+                if (!in.contains(c.getTarget()) && d.get(c.getTarget()) > time + c.getTimeCost()) {
+                    d.put(c.getTarget(), time + c.getTimeCost());
+                    p.put(c.getTarget(), stop);
+                }
+            }
+        }
+
+        for (Stop stop = target;; stop = p.get(stop)) {
+            path.add(stop);
+            if (!p.containsKey(stop)) {
+                break;
+            }
+        }
+
+        return path;
     }
 
     public StopFactory getStopFactory() {
