f8c90b5187b801dd8cfa2a9a41ce4cf348114d52
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-05 13:11:49 -0300

    Javadoc da interface Location

--- onibuscerto-api/src/main/java/com/onibuscerto/api/entities/Location.java
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/entities/Location.java
@@ -1,12 +1,32 @@
 package com.onibuscerto.api.entities;
 
+/**
+ * Interface que define um tipo de entidade com uma posição no mapa. A posição
+ * no mapa é dada pelo par latitude e longitude, em graus.
+ */
 public interface Location {
 
+    /**
+     * Retorna a latitude da entidade.
+     * @return latitude em graus
+     */
     public double getLatitude();
 
+    /**
+     * Atribui a latitude da entidade.
+     * @param latitude latitude em graus
+     */
     public void setLatitude(double latitude);
 
+    /**
+     * Retorna a longitude da entidade.
+     * @return longitude em graus
+     */
     public double getLongitude();
 
+    /**
+     * Atribui a longitude da entidade.
+     * @param longitude longitude em graus
+     */
     public void setLongitude(double longitude);
 }
---
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/utils/GlobalPosition.java
@@ -0,0 +1,31 @@
+package com.onibuscerto.api.utils;
+
+/**
+ * Um par latitude, longitude representando uma posição global.
+ */
+public final class GlobalPosition {
+
+    private double latitude;
+    private double longitude;
+
+    public GlobalPosition(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
