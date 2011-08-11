620e6cdb3dd116503b234155a0a2f58a093ddba3
Bruno Weingraber <brunoweingraber@gmail.com>
2011-08-11 15:30:48 -0300

    Interfaces Calendar e CalendarFactory criadas.

---
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/entities/Calendar.java
@@ -0,0 +1,61 @@
+package com.onibuscerto.api.entities;
+
+import java.util.Collection;
+
+public interface Calendar {
+
+    /**
+     * Retorna o id que identifica as datas em que o serviço estará disponível
+     * para um ou mais trajetos.
+     * @return id do service
+     */
+    public String getServiceId();
+
+    /**
+     * Retorna o id que identifica as datas em que o serviço estará disponível
+     * @param serviceId id do service
+     */
+    public void setServiceId(String serviceId);
+
+    /**
+     * Retorna um vetor contendo a disponibilidade de serviço por dia da semana
+     * @return disponibilidade do serviço por dia da semana:
+     * 0: indisponível
+     * 1: disponível
+     */
+    public Collection<String> getDaysOfWeek();
+
+    /**
+     * Atribui um vetor contendo a disponibilidade de serviço por dia da semana
+     * @param days vetor com a disponibilidade do serviço por dia da semana:
+     * 0: indisponível
+     * 1: disponível
+     */
+    public void setDaysOfWeek(Collection<String> days);
+
+    /**
+     * Retorna a data de início do serviço.
+     * @return data de início do serviço.
+     */
+    public String getStartDate();
+
+    /**
+     * Atribui a data de início do serviço. O valor do campo startDate deve
+     * estar no formato AAAAMMDD.
+     * @param startDate data de início do serviço.
+     */
+    public void setStartDate(String startDate);
+
+    /**
+     * Retorna a data de término do serviço.
+     * @return data de término do serviço.
+     */
+    public String getEndDate();
+
+    /**
+     * Atribui a data de término do serviço. O valor do campo endDate deve
+     * estar no formato AAAAMMDD.
+     * @param endDate data de início do serviço.
+     */
+    public void setEndDate(String endDate);
+}
---
+++ onibuscerto-api/src/main/java/com/onibuscerto/api/factories/CalendarFactory.java
@@ -0,0 +1,8 @@
+package com.onibuscerto.api.factories;
+
+import java.util.Calendar;
+
+public interface CalendarFactory {
+
+    public Calendar createCalendar(String serviceId);
+}
